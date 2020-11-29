09cc8cb3b155b755907d28b76ca8a9687a7f6e1b
Xidron <maksymilian.mrozowski@student.uw.edu.pl>
2020-11-29 03:47:05 +0100

    lvl2 transition

---
+++ NoBorders/Assets/Scripts/TransitionControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionControl : MonoBehaviour
+{
+
+    public Rigidbody2D myRB;
+
+    public void SetKinematic()
+    {
+        myRB.isKinematic = true;
+    }
+    public void SetDynamic()
+    {
+        myRB.isKinematic = false;
+    }
+
+}
