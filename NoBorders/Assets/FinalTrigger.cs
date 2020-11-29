3bad4077377b664f0dc05179566359ecd5a04ecc
Xidron <maksymilian.mrozowski@student.uw.edu.pl>
2020-11-29 07:28:43 +0100

    Final Changes Max

---
+++ NoBorders/Assets/FinalTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class FinalTrigger : MonoBehaviour
+{
+    public PlayableDirector director;
+    public Rigidbody2D rb;
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                // Trigger some animation
+                rb.isKinematic = true;
+                director.Play();
+
+            }
+        }
+    }
+}
--- NoBorders/Assets/Scripts/Fade.cs
+++ NoBorders/Assets/Scripts/Fade.cs
@@ -5,21 +5,32 @@ using UnityEngine.UI;
 public class Fade : MonoBehaviour
 {
     Image img => GetComponent<Image>();
-    public float duration = 1f;
+    public float duration = 3f;
 
     public void FadeOut()
     {
-        StartCoroutine(FadeImage());
+        StartCoroutine(FadeImageOut());
     }
- 
-    IEnumerator FadeImage()
+
+    public void FadeIn()
+    {
+        StartCoroutine(FadeImageIn());
+    }
+
+    IEnumerator FadeImageOut()
     {
         for (float i = 0; i <= 1; i += Time.deltaTime / duration) {
             img.color = new Color(img.color.r, img.color.g, img.color.b, i);
             yield return null;
         }
 
-        for (float i = 1; i >= 0; i -= Time.deltaTime / duration) {
+    }
+
+    IEnumerator FadeImageIn()
+    {
+        
+        for (float i = 1; i >= 0; i -= Time.deltaTime / duration)
+        {
             img.color = new Color(img.color.r, img.color.g, img.color.b, i);
             yield return null;
         }
