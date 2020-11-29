2a247f1e2ebcfc46c23e57fe47515ec5c2b102f6
Michal Waszczuk <ihoold@gmail.com>
2020-11-29 01:56:40 +0100

    Deleted unnecessary assets, started collectible system

---
+++ NoBorders/Assets/Scripts/ReturnZone.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnZone : MonoBehaviour
+{
+    void OnTriggerStay2D(Collider2D other) {
+        if (other.tag == "Player") {
+            if (Input.GetKeyDown(KeyCode.E)) {
+                // Trigger some animation of returning and enable way to next collectable
+		    }
+        }
+    }
+}
